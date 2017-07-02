f7a8f23c5e4a1f4b6b016908403a179b752c425e
Justin Lin <caterpillar@openhome.cc>
2017-07-02 12:02:02 +0800

    a initial case

---
+++ src/cc/openhome/CLIDemo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc.openhome;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author Justin
+ */
+public class CLIDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+
+    public static void commandLine(String[] arguments)  {
+        if(arguments.length != 0 && "-help".equals(arguments[0])) {
+            out.println("show help");
+        }
+    }
+}
